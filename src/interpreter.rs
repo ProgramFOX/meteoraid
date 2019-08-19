2f9df1fb7de630f6ce6f2a7b9dc5d1ef1e9c702d
Thomas Daniels <daniels.thomas@pm.me>
2019-08-20 00:57:35 +0200

    Add Interpreter to deal with timestamps and checkpoints

---
+++ src/interpreter.rs
@@ -0,0 +1,225 @@
+use crate::builder::{BuilderError, SessionBuilder};
+use crate::lua;
+use crate::session::*;
+use crate::timestamp::Timestamp;
+use rlua::Lua;
+
+pub struct Interpreter {
+    session_builder: SessionBuilder,
+    time_checkpoint: Option<Timestamp>,
+    lua: Lua,
+}
+
+impl Interpreter {
+    pub fn new() -> Result<Interpreter, rlua::Error> {
+        Ok(Interpreter {
+            session_builder: SessionBuilder::new(),
+            time_checkpoint: None,
+            lua: lua::new_lua()?,
+        })
+    }
+
+    pub fn execute_one_line(&mut self, line: &str) -> Result<(), Box<dyn std::error::Error>> {
+        let code = line
+            .split("--")
+            .into_iter()
+            .next()
+            .expect("[supposedly unreachable] Split has no values")
+            .trim();
+
+        if code.len() == 0 {
+            return Ok(());
+        }
+
+        let mut split = code.split("<<").into_iter();
+        let code = split
+            .next()
+            .expect("[supposedly unreachable] Split has no values (2)")
+            .trim();
+        let maybe_exact_timestamp = match split.next() {
+            Some(time) => Some(Timestamp::from_shorthand_int_notation(
+                time.trim().parse::<u32>()?,
+            )),
+            None => None,
+        };
+
+        match code.parse::<u32>() {
+            Ok(number) => {
+                self.time_checkpoint = Some(Timestamp::from_shorthand_int_notation(number));
+            }
+            _ => {
+                if let Some(exact_timestamp) = maybe_exact_timestamp {
+                    self.time_checkpoint = Some(exact_timestamp);
+                }
+                match self.time_checkpoint {
+                    Some(time) => {
+                        let time_and_event =
+                            TimestampedEvent(time, lua::run_code(code, &self.lua)?);
+                        self.session_builder.register_event(time_and_event)?;
+                    }
+                    None => Err(InterpreterError::NoTimeCheckpoint)?,
+                }
+            }
+        };
+        Ok(())
+    }
+
+    pub fn execute_multiple_lines(&mut self, code: &str) -> Result<(), Box<dyn std::error::Error>> {
+        for line in code.split('\n') {
+            self.execute_one_line(line)?;
+        }
+        Ok(())
+    }
+
+    pub fn get_session(mut self) -> Result<Session, BuilderError> {
+        self.session_builder.to_session()
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum InterpreterError {
+    NoTimeCheckpoint,
+}
+
+impl std::fmt::Display for InterpreterError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                InterpreterError::NoTimeCheckpoint => "No time checkpoint has been given.",
+            }
+        )
+    }
+}
+
+impl std::error::Error for InterpreterError {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_interpreter_1() {
+        let mut interpreter = Interpreter::new().unwrap();
+        interpreter
+            .execute_multiple_lines(
+                "2237
+                 period_start
+                 clouds(0)
+                 showers(PER, ANT, KCG, SPO)
+                 areas(area14(11))
+                 fieldC(336, 52.3)
+                 2337
+                 period_end",
+            )
+            .unwrap();
+        let session = interpreter.get_session().unwrap();
+        let period = &session.periods[0];
+        assert_eq!(period.teff, 1.0);
+        assert_eq!(period.showers.len(), 4);
+        assert_eq!(period.cloud_factor, 1.0);
+    }
+
+    #[test]
+    fn test_interpreter_2() {
+        let mut interpreter = Interpreter::new().unwrap();
+        interpreter
+            .execute_multiple_lines(
+                "period_start << 2237
+                 clouds(0)
+                 showers(PER, ANT, KCG, SPO)
+                 areas(area14(11))
+                 fieldC(336, 52.3)
+                period_end << 2337",
+            )
+            .unwrap();
+        let session = interpreter.get_session().unwrap();
+        let period = &session.periods[0];
+        assert_eq!(period.teff, 1.0);
+        assert_eq!(period.showers.len(), 4);
+        assert_eq!(period.cloud_factor, 1.0);
+    }
+
+    #[test]
+    fn test_interpreter_3() {
+        let mut interpreter = Interpreter::new().unwrap();
+        assert!(interpreter
+            .execute_multiple_lines(
+                "period_start
+                 clouds(0)
+                 showers(PER, ANT, KCG, SPO)
+                 areas(area14(11))
+                 fieldC(336, 52.3)
+                 period_end << 2337",
+            )
+            .is_err());
+    }
+
+    #[test]
+    fn test_interpreter_4() {
+        let mut interpreter = Interpreter::new().unwrap();
+        interpreter
+            .execute_multiple_lines(
+                "period_start << 2237
+                 clouds(0)
+                 showers(PER, ANT, KCG, SPO)
+                 areas(area14(11))
+                 fieldC(336, 52.3)
+                 2307
+                 areas(area14(9))
+                 clouds(10)
+                 per(3.5)
+                 period_end << 2337",
+            )
+            .unwrap();
+        let session = interpreter.get_session().unwrap();
+        let period = &session.periods[0];
+        assert_eq!(period.teff, 1.0);
+        assert_eq!(period.showers.len(), 4);
+        assert_eq!(period.cloud_factor, 1.05);
+        assert_eq!(period.limiting_magnitude, 5.52);
+        assert_eq!(period.meteors.len(), 1);
+    }
+
+    #[test]
+    fn test_interpreter_5() {
+        let mut interpreter = Interpreter::new().unwrap();
+        interpreter
+            .execute_multiple_lines(
+                "period_start << 2237
+                 clouds(0)
+                 showers(PER, ANT, KCG, SPO)
+                 areas(area14(11))
+                 fieldC(336, 52.3)
+                 2307
+                 areas(area14(9))
+                 clouds(10)
+                 per(3.5)
+                 period_end << 2337
+                 
+                 new_period
+
+                 0015
+                 period_start
+                 clouds(5)
+                 showers(PER, SPO)
+                 areas(area14(10), area7(10))
+                 fieldC(298, 56)
+                 period_end << 0030
+                 ",
+            )
+            .unwrap();
+        let session = interpreter.get_session().unwrap();
+        assert_eq!(session.periods.len(), 2);
+        let period = &session.periods[0];
+        assert_eq!(period.teff, 1.0);
+        assert_eq!(period.showers.len(), 4);
+        assert_eq!(period.cloud_factor, 1.05);
+        assert_eq!(period.limiting_magnitude, 5.52);
+        assert_eq!(period.meteors.len(), 1);
+
+        let period2 = &session.periods[1];
+        assert_eq!(period2.teff, 0.25);
+    }
+}
